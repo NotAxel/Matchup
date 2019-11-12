f1724999b2962ab7c387af44832c596686d3abb6
NotAxel <titouan.magret@gmail.com>
2019-11-11 16:28:19 -0800

    able to signup users

---
+++ lib/User.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+/** This is the default user class that each user will have. The information for this 
+ * will be pulled form the firebase server.
+ */
+
+int MAX_FRIENDS = 31;
+
+class User {
+  String email;
+  String password;
+  String userName; // Players username
+  String main; // Players main character
+  String secondary; // Players secondary
+  String region; // Players region they are located in
+  var friendsList = new List(MAX_FRIENDS); // Players will have a max friend count
+  int friendCount = 0; // Players friend count
+
+
+  //Getters
+  String get getName {
+    return userName;
+  }
+
+  String get getMain {
+    return main;
+  }
+
+  String get getSecondary {
+    return secondary;
+  }
+
+  String get getRegion {
+    return region;
+  }
+  
+  int get getFriendCount {
+    return friendCount;
+  }
+
+  /// Adding a friend to this users friends list
+  void addFriend (User friend){
+    if(friendCount < MAX_FRIENDS) {
+      friendsList[getFriendCount] = friend;
+      friendCount = friendCount + 1;
+    } else {
+    
+    }
+  }
+
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'authentication.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,30 +22,48 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(title: 'Matchup Demo Home Page'),
+      home: MyHomePage(title: 'Matchup Demo Home Page 3'),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState(){
-    Firestore.instance.collection('Players').document()
-      .setData({ 'Username': 'Brendan', 'Main': 'Mr. Game & Watch' });
+    Auth auth = new Auth();
+    auth.signUp("gpoole@calpoly.edu", "IsuckHa");
+    //Firestore.instance.collection('Players').document()
+      //.setData({ 'Username': 'Brendan', 'Main': 'Mr. Game & Watch' });
     super.initState();
   }
   /*
   int _counter = 0;
+
   void _incrementCounter() {
     setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -52,14 +71,38 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: new Text("Testing firebase"),
      /* 
       Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -76,7 +119,7 @@ class _LoginPageState extends State<LoginPage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), 
+      ), // This trailing comma makes auto-formatting nicer for build methods.
       */
     );
   }
