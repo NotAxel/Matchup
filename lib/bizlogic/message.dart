662ddc198d2d5daf77f8ce76574a0bda59d941aa
Harmless19 <43099759+Harmless19@users.noreply.github.com>
2020-01-08 15:44:47 -0800

    created message class to be used in firebase messaging

---
+++ lib/bizlogic/message.dart
@@ -0,0 +1,30 @@
+class Message{
+
+  String _content;
+  String _toId;
+  String _fromId;
+  DateTime _timeStamp;
+  
+  Message(String content, String toId, String fromId){
+    _content = content;
+    _toId = toId;
+    _fromId = fromId;
+    _timeStamp = DateTime.now();
+  }
+
+  // content
+  String get getContent => _content;
+  set setContent(String content){ _content = content; } 
+
+  // toId
+  String get getToId => _toId;
+  set setToId(String toId){ _toId = toId; } 
+
+  // fromId 
+  String get getFromId => _fromId;
+  set setFromId(String fromId){ _fromId = fromId; } 
+
+  // timeStamp
+  get timeStamp => _timeStamp;
+
+}
