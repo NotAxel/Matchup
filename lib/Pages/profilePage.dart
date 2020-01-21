96c8880c2fa7926c5875a8632b254a02f9d81730
Brendan Dunning <bbdunning@gmail.com>
2020-01-21 15:57:01 -0800

    added character renders

--- lib/Pages/profilePage.dart
+++ lib/Pages/profilePage.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:collection';
 import 'package:matchup/Pages/homepage.dart';
 import 'package:matchup/bizlogic/User.dart';
 import 'package:matchup/bizlogic/userProvider.dart';
+
+Map<String, String> nameMap = {
+  "Mario" : 'assets/images/characterPortraits/1.png',
+  "Donkey Kong" : 'assets/images/characterPortraits/2.png',
+  "Link" : 'assets/images/characterPortraits/3.png',
+  "Samus" : 'assets/images/characterPortraits/4.png',
+  "Dark Samus" : 'assets/images/characterPortraits/5.png',
+  "Yoshi" : 'assets/images/characterPortraits/6.png',
+  "Kirby" : 'assets/images/characterPortraits/7.png',
+  "Fox" : 'assets/images/characterPortraits/8.png',
+  "Pikachu" : 'assets/images/characterPortraits/9.png',
+  "Luigi" : 'assets/images/characterPortraits/10.png',
+  "Ness" : 'assets/images/characterPortraits/11.png',
+  "Captain Falcon" : 'assets/images/characterPortraits/1.png',
+  "Jigglypuff" : 'assets/images/characterPortraits/1.png',
+  "Peach" : 'assets/images/characterPortraits/1.png',
+  "Daisy" : 'assets/images/characterPortraits/1.png',
+  "Bowser" : 'assets/images/characterPortraits/1.png',
+  "Ice Climbers" : 'assets/images/characterPortraits/1.png',
+  "Sheik" : 'assets/images/characterPortraits/1.png',
+  "Zelda" : 'assets/images/characterPortraits/1.png',
+  "Dr. Mario" : 'assets/images/characterPortraits/1.png',
+  "Pichu" : 'assets/images/characterPortraits/1.png',
+  "Falco" : 'assets/images/characterPortraits/1.png',
+  "Marth" : 'assets/images/characterPortraits/1.png',
+  "Lucina" : 'assets/images/characterPortraits/1.png',
+  "Young Link" : 'assets/images/characterPortraits/1.png',
+  "Ganondorf" : 'assets/images/characterPortraits/1.png',
+  "Mewtwo" : 'assets/images/characterPortraits/1.png',
+  "Roy" : 'assets/images/characterPortraits/1.png',
+  "Chrom" : 'assets/images/characterPortraits/1.png',
+  "Mr. Game & Watch" : 'assets/images/characterPortraits/1.png',
+};
 
 class ProfilePage extends StatefulWidget {
   final VoidCallback logoutCallback;
@@ -62,7 +96,8 @@ class ProfilePageState extends State<ProfilePage> with SingleTickerProviderState
             //Should get username from firebase
             Text(''),
             //Should get profile pic from firebase
-            Center(child: Image.asset('assets/images/default_profile.jpg', height: 300)),
+/*             Center(child: Image.asset('assets/images/default_profile.jpg', height: 300)), */
+            Center(child: Image.asset(nameMap["Donkey Kong"], height: 300)),
             Container(height: 50),
             //Should get mains from firebase
 //            Text(_user.getMain, style: profStyle),
