7683483524456c4f26607c31c9916f940a9d1c19
Brendan Dunning <bbdunning@gmail.com>
2020-01-22 14:48:21 -0800

    merged with axel stuff

--- lib/Pages/profilePage.dart
+++ lib/Pages/profilePage.dart
@@ -97,7 +97,7 @@ class ProfilePageState extends State<ProfilePage> with SingleTickerProviderState
             Text(''),
             //Should get profile pic from firebase
 /*             Center(child: Image.asset('assets/images/default_profile.jpg', height: 300)), */
-            Center(child: Image.asset(nameMap["Donkey Kong"], height: 300)),
+            Center(child: Image.asset(nameMap[_user.getMain], height: 300)),
             Container(height: 50),
             //Should get mains from firebase
 //            Text(_user.getMain, style: profStyle),
