36f13aab7e193c8ad741cb6d02ee8412a9a3e24c
Kyle Harms <43099759+Harmless19@users.noreply.github.com>
2020-01-26 23:45:32 -0800

    wrote first tests for chat page logic

--- test/unittest/chatPageLogic_test.dart
+++ test/unittest/chatPageLogic_test.dart
@@ -3,6 +3,29 @@ import 'package:test/test.dart';
 import 'package:matchup/bizlogic/chatPageLogic.dart';
 
 void main(){
-  test('', (){
+  test('user messages are placed on the right of message rows', (){
+    // ARRANGE
+    MainAxisAlignment expectedAllignment = MainAxisAlignment.end;
+    bool isUserMessage = true;
+    MainAxisAlignment actualAllignment;
+
+    // ACT
+    actualAllignment = ChatPageLogic.rowMainAxisAlignment(isUserMessage);
+
+    // ASSERT
+    expect(actualAllignment, expectedAllignment);
+  });
+
+  test('peer messages are placed on the right of message rows', (){
+    // ARRANGE
+    MainAxisAlignment expectedAllignment = MainAxisAlignment.start;
+    bool isUserMessage = false;
+    MainAxisAlignment actualAllignment;
+
+    // ACT
+    actualAllignment = ChatPageLogic.rowMainAxisAlignment(isUserMessage);
+
+    // ASSERT
+    expect(actualAllignment, expectedAllignment);
   });
 }
