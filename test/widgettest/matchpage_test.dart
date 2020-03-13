a862347fa138c0f6984541014fae216b1378dd4a
Harmless19 <43099759+Harmless19@users.noreply.github.com>
2020-03-12 23:57:55 -0700

    working on merging testing

--- test/widgettest/matchpage_test.dart
+++ test/widgettest/matchpage_test.dart
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -70,4 +68,3 @@ void main() {
     expect(finder, findsOneWidget);
   });
 }
->>>>>>> master
--- test/widgettest/rootPage_test.dart
+++
@@ -1,1 +0,0 @@
-// root page test
